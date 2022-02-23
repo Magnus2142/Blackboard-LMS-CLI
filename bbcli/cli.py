5a8c1e83efcce12ba92afc6eaa09a54513480ab1
williamforbrigd <hansw0701@gmail.com>
2022-02-23 12:56:05 +0100

    fixed .gitignore and added endpoints

--- bbcli/cli.py
+++ bbcli/cli.py
@@ -14,6 +14,8 @@ def _version_callback(value: bool) -> None:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
 
+
+
 @app.callback()
 def main(
     version: Optional[bool] = typer.Option(
