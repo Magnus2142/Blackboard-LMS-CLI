9b7c788062e039493fee1749dd4ceaea830c2ed5
Mattias Eggen <mattias.a.eggen@gmail.com>
2022-04-04 15:54:04 +0200

    Working get_assignments

--- bbcli/commands/assignments.py
+++ bbcli/commands/assignments.py
@@ -4,10 +4,10 @@ import click
 from bbcli.services import assignment_service
 
 @click.command(name='get')
-@click.argument('course_id', required=True, default='_27251_1')
+@click.argument('course_id', required=True)
 @click.pass_context
 def get_assignments(ctx, course_id):
     """
     Get assignments
     """
-    response = assignment_service.get_assignments(ctx.obj['SESSION'], course_id)
+    assignment_service.get_assignments(ctx.obj['SESSION'], course_id)
