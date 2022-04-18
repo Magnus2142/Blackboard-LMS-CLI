9d778e48c33ab53e6d69a0e86e65cab4b5fc323d
williamforbrigd <hansw0701@gmail.com>
2022-04-18 12:49:51 +0200

    small improvement

--- bbcli/views/contents_view.py
+++ bbcli/views/contents_view.py
@@ -9,24 +9,22 @@ def list_tree(root, folder_ids, node_ids):
     # color = Fore.RESET if only_folders else Fore.BLUE
     color = Fore.BLUE
     for pre, fill, node in RenderTree(root):
-        node_id = None
-        if 'node_id' in dir(node):
-            node_id = getattr(node, 'node_id')
-        else:
-            click.echo('Could not retreive the node id.')
+        node_id = node.name.split()[0]
         if node_id in folder_ids:
-            click.echo(f'{pre}{color}{node_id} {node.name} {Style.RESET_ALL}')
+            click.echo(f'{pre}{color} {node.name} {Style.RESET_ALL}')
         elif node_id in node_ids:
-            click.echo(f'{pre}{node_id} {node.name}')
+            click.echo(f'{pre} {node.name}')
         else:
             click.echo('Neither node nor folder.')
+
+
         
 
 def open_vim(data):
     str = data['title'] + '\n'
     str += html_to_text(data['body'])
 
-    EDITOR = os.environ.get('EDITOR','vim') #that easy!
+    EDITOR = os.environ.get('EDITOR','vim') 
 
     initial_message = bytearray(str, encoding='utf8')
 
